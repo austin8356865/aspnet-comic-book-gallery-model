0ff87df324018471f2e394332662cad739443434
Austin Adams <austin.adams@kizan.com>
2018-05-22 16:53:03 -0400

    Made  a lot of changes!

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -0,0 +1,44 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                var series = new Series()
+                {
+                    Title = "The Amazing Spider-Man"
+                };
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    Series = series,
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    Series = series,
+                    IssueNumber = 2,
+                    PublishedOn = DateTime.Today
+                });
+
+                context.SaveChanges();
+                var comicBooks = context.ComicBooks.Include(cb => cb.Series).ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
